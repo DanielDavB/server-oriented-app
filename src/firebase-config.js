aea2e3396bf28b222bd058cfcd676ab16ebe95d9
DanielDavB <danieldavilabarrios@gmail.com>
2023-10-12 00:32:03 -0600

    Firebase_config

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBSkwv2jHb1n2nl8YregMpUkjIogTUnI_E",
+    authDomain: "fproject-94c46.firebaseapp.com",
+    projectId: "fproject-94c46",
+    storageBucket: "fproject-94c46.appspot.com",
+    messagingSenderId: "930723996084",
+    appId: "1:930723996084:web:ccfd17d1296b7a8c1652e1",
+    measurementId: "G-CBJ4FPB56Y"
+  };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // Import the App component
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import firebaseConfig from './firebase-config';
+import{
+  FirebaseAppProvider 
+} from 'reactfire'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App /> {/* Render the App component */}
-  </React.StrictMode>,
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App /> {/* Render the App component */}
+    </FirebaseAppProvider>,
   document.getElementById('root')
 );
